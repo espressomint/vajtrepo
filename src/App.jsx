751cf4b0a96b2f10b118a9166064acce0a75ffa0
espressomint <espressomint06@gmail.com>
2024-10-03 10:39:20 +0200

    Add css

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
+        <a className="bigLink" href="https://www.youtube.com/watch?v=ykOEauDxFkQ">
+          <div className="bigLinkBackground"></div>
+          heyo
+
+        </a>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
